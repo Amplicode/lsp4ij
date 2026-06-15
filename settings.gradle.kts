dede9b46ef6ef8e879c07194f7870f75854bb108
subbotin <a.subbotin@haulmont.com>
2026-06-15 16:46:20 +0400

    add repositories

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,5 @@
 pluginManagement {
     repositories {
-        maven("https://cache-redirector.jetbrains.com/plugins.gradle.org/m2")
         maven("https://cache-redirector.jetbrains.com/repo1.maven.org/maven2")
         gradlePluginPortal()
         mavenCentral()
